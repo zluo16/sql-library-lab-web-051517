1e1ace02470832c32d9138d428be2547d9566629
zluo16 <zluo@alumni.berklee.edu>
2017-05-23 08:54:36 -0400

    finished library

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,29 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT characters.species, COUNT(characters.species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series ON
+  authors.id = series.author_id INNER JOIN subgenres ON
+  series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title FROM series JOIN characters ON
+  series.id = characters.series_id GROUP BY species ORDER BY
+  COUNT(species) LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters JOIN
+  character_books ON characters.id = character_books.character_id GROUP BY
+  characters.name ORDER BY COUNT(book_id) DESC"
 end
